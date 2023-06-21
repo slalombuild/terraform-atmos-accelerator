71a81c033b254483a6cd941b61187cb779066686
girish.maddineni <girish.maddineni@slalom.com>
2023-06-21 16:12:45 -0700

    fixed the conditions for all for_each by integrating module.enabled

--- components/terraform/gcp/gcs-bucket/kms.tf
+++ components/terraform/gcp/gcs-bucket/kms.tf
@@ -1,13 +1,13 @@
 resource "google_kms_key_ring" "key_ring" {
   count    = local.enabled && var.kms_encryption_enabled ? 1 : 0
   project  = var.project_id
-  name     = "${module.this.id}-temp"
+  name     = "${module.this.id}-${var.bucket_name}"
   location = lower(var.kms.location)
 }
 
 resource "google_kms_crypto_key" "crypto_key" {
   count           = local.enabled && var.kms_encryption_enabled ? 1 : 0
-  name            = "${module.this.id}temp"
+  name            = "${module.this.id}-${var.bucket_name}"
   key_ring        = google_kms_key_ring.key_ring[0].id
   rotation_period = var.kms.key_rotation_period
   labels          = module.this.additional_tag_map
